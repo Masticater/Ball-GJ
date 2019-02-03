ebba2a3e81c35d4ac262313cfbf4d2ebf5a22997
Masticater <30707218+Masticater@users.noreply.github.com>
2019-02-02 21:19:22 -0500

    Just Getting Started
    Uploading whatever it is that I'm making. Just kinda messing around for now.

---
+++ Assets/Scripts/MyCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCamera : MonoBehaviour
+{
+    Vector3 offset;
+    public Transform target;
+
+	void Start()
+	{
+        offset = target.position - transform.position;
+	}
+	
+
+    void Update()
+    {
+        transform.position = target.transform.position - offset;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Vector3 xMovement, yMovement;
+    public float speed = 5, maxSpeed, jumpForce;
+    Rigidbody rb;
+    public bool hasJumped = false;
+
+    void Start()
+	{
+        rb = GetComponent<Rigidbody>();
+	}
+	
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float xMovement = Input.GetAxisRaw("Horizontal");
+        float yMovement = Input.GetAxisRaw("Vertical");
+
+        rb.AddForce(new Vector3(xMovement, 0, yMovement) * speed);
+        if (rb.velocity.x > maxSpeed)
+            rb.velocity = new Vector3(maxSpeed, rb.velocity.y, rb.velocity.z);
+        if(rb.velocity.x < -maxSpeed)
+            rb.velocity = new Vector3(-maxSpeed, rb.velocity.y, rb.velocity.z);
+        if (rb.velocity.z > maxSpeed)
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, maxSpeed);
+        if (rb.velocity.z < -maxSpeed)
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -maxSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && !hasJumped)
+        {
+            hasJumped = true;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        hasJumped = false;
+    }
+}
