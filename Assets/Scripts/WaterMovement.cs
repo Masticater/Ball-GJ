92f8804bdd86b3400c4adafe6e3d0a1f6bb1c922
Masticater <30707218+Masticater@users.noreply.github.com>
2019-02-04 18:52:07 -0500

    Changed Project
    Changed to a 2D "Captain Planet" game.

---
+++ Assets/Scripts/WaterMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMovement : MonoBehaviour
+{
+    public float speed;
+
+	void Start()
+	{
+	
+	}
+	
+    void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+        pos -= new Vector2(speed, 0) * Time.deltaTime;
+        transform.position = pos;
+        
+        if (transform.position.x <= -7f)
+            transform.position = new Vector2 (7f, transform.position.y);
+    }
+}
