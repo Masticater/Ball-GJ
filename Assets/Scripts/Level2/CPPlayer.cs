d9bf1be3eab7a74a6e7b5ec6a3fb232473da8920
Masticater <30707218+Masticater@users.noreply.github.com>
2019-05-05 18:45:00 -0400

    Added CP Animations
    Added/corrected animations & some non-functioning movement script

---
+++ Assets/Scripts/Level2/CPPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPPlayer : MonoBehaviour
+{
+    Animator anim;
+    float horixontalMove;
+    float moveSpeed = 1f;
+    bool punching;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horixontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        UpdateAnim();
+    }
+
+    void UpdateAnim()
+    {
+        anim.SetFloat("xDir", Mathf.Abs(horixontalMove));
+
+        if (Input.GetKeyDown(KeyCode.Space) && !punching)
+        {
+            anim.SetBool("Punching", true);
+            punching = true;
+            StartCoroutine(ResetAnim());
+        }
+    }
+
+    IEnumerator ResetAnim()
+    {
+        float clipLength = .5f;
+        yield return new WaitForSeconds(clipLength);
+        anim.SetBool("Punching", false);
+        punching = false;
+    }
+}
