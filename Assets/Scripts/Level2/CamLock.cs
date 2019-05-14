edd508d5fe98f655544196a181518582672817de
Masticater <30707218+Masticater@users.noreply.github.com>
2019-05-13 22:19:51 -0400

    Finished Level Design
    Completed the level environment, and positioning everything. All colliders and decorations should be correct now.

---
+++ Assets/Scripts/Level2/CamLock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamLock : MonoBehaviour
+{
+    public Cinemachine.CinemachineVirtualCamera cam;
+    public GameObject[] boss;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        cam.Follow = gameObject.transform;
+        GetComponent<BoxCollider2D>().isTrigger = false;
+
+        foreach (GameObject bossGO in boss)
+        {
+            bossGO.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Level2/Sludge.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sludge : MonoBehaviour
+{
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        
+    }
+}
